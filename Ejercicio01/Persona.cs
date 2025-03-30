dfd5d72e3154976de4129c72a749239a2bce71c8
dpividori <dpividori@gmail.com>
2025-03-30 13:55:42 -0300

    V1

---
+++ Ejercicio01/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    abstract class Persona
+    {
+        String _nombre;
+        String _apellido;
+        DateTime _fechaNacimiento;
+
+        protected abstract void GetCredencial();
+        protected virtual void GetNombreCompleto()
+        {
+            Console.WriteLine("Nombre: " + _nombre + " " + _apellido);
+        }
+        protected abstract void getSaludoInformal();
+
+
+    }
+}
