e378afd8bf4201e4affb86a6c23d20f2c70837ba
aprendo1 <cesarloiacono1@gmail.com>
2025-03-30 16:04:22 -0300

    clase empleado

---
+++ Ejercicio01/Empleado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Empleado : Persona
+    {
+
+        // ATRIBUTOS
+        private DateTime fechaIngreso;
+        private int legajo;
+
+
+        // PROPIEDADES
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public int Legajo { get => legajo; set => legajo = value; }
+
+        // PROPIEDADES HEREDADAS
+        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
+
+        // propiedades custom
+        public int Antiguedad { get => _legajo; set => _legajo = value; }
+
+        // Métodos: implemento porque debo hacerlo
+        public override void GetCredencial()
+        {
+            Console.WriteLine("Credencial de empleado");
+        }
+    }
+}  
+    
